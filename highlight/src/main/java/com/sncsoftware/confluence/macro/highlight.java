1fd61484d592d061245df2b3dfcbe96fe1d3e04d
Michael Slattery <mslattery@fulcrumit.com>
2017-12-18 13:42:30 -0500

    Added TODO and some early basics

--- highlight/src/main/java/com/sncsoftware/confluence/macro/highlight.java
+++ highlight/src/main/java/com/sncsoftware/confluence/macro/highlight.java
@@ -3,11 +3,19 @@ package com.sncsoftware.confluence.macro;
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
+import com.atlassian.confluence.macro.ResourceAware;
+import com.atlassian.confluence.setup.settings.SettingsManager;
 
 import java.util.Map;
 
 /*@Scanned*/
-public class highlight implements Macro {
+public class highlight implements Macro, ResourceAware {
+
+	private final SettingsManager settingsManager;
+    public highlight(SettingsManager settingsManager)
+    {
+        this.settingsManager = settingsManager;
+    }
 
     public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
         return ("<span style=\"background-color: " + map.get("Color") + ";\">" + map.get("Text") +"</span>");
@@ -16,4 +24,7 @@ public class highlight implements Macro {
     public BodyType getBodyType() { return BodyType.NONE; }
 
     public OutputType getOutputType() { return OutputType.BLOCK; }
+
+  	public String getResourcePath() { return null; }
+    public void setResourcePath(String s) {}
 }
