7720bd6cbe96eada9377aa6f155d4218cb2b1add
Michael J. Slattery <mslattery@sncsoftware.com>
2017-12-18 16:17:23 -0500

    Fixed comment

--- highlight/src/main/java/com/sncsoftware/confluence/macro/highlight.java
+++ highlight/src/main/java/com/sncsoftware/confluence/macro/highlight.java
@@ -6,10 +6,8 @@ import com.atlassian.confluence.macro.MacroExecutionException;
 
 import java.util.Map;
 
-/*@Scanned*/
 public class highlight implements Macro {
 
-<<<<<<< HEAD
     private static final String PARAM_NAME_TEXT = "Text";
     private static final String PARAM_NAME_COLOR = "Color";
     private static final String PARAM_NAME_CUSTOM_COLOR = "Custom Color";
@@ -21,17 +19,6 @@ public class highlight implements Macro {
  			return ("<span style=\"background-color: " + params.get(PARAM_NAME_CUSTOM_COLOR) + ";\">" + params.get(PARAM_NAME_TEXT) +"</span>");
  		}
     	return ("<span style=\"background-color: " + params.get(PARAM_NAME_COLOR) + ";\">" + params.get(PARAM_NAME_TEXT) +"</span>");
-=======
-    private static final String PARAM_NAME = "Custom Color";
-
-    public String execute(Map<String, String> params, String s, ConversionContext conversionContext) throws MacroExecutionException {
-
- 		if (params.containsKey(PARAM_NAME))
- 		{
- 			return ("<span style=\"background-color: " + params.get(PARAM_NAME) + ";\">" + params.get("Text") +"</span>");
- 		}
-    	return ("<span style=\"background-color: " + params.get("Color") + ";\">" + params.get("Text") +"</span>");
->>>>>>> 0f9a6d91942faa681edfe1e60ee585f02b998c83
      }
 
     public BodyType getBodyType() { return BodyType.NONE; }
